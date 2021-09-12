b0d4e5654e0556f0004d5390846088b5b9a42d45
BalajiHarinath <89400019+BalajiHarinath@users.noreply.github.com>
2021-09-13 01:18:22 +0530

    feat : initial commit for istriangle.html and istriangle.js

---
+++ istriangle.js
@@ -0,0 +1,17 @@
+const angles = document.querySelectorAll(".angles");
+const button = document.querySelector("#btn-angle");
+const output = document.querySelector("#message");
+
+function calculateSum(a,b,c){
+    const sumA = a+b+c;
+    return sumA;
+};
+function clickHandlerAngles(){
+    const sumAngles = calculateSum(Number(angles[0].value), Number(angles[1].value), Number(angles[2].value));
+    if(sumAngles === 180){
+        output.innerText = "Yay!! it is a triangle."
+    }else{
+        output.innerText = "Oops!! it is not a triangle."
+    }
+};
+button.addEventListener("click",clickHandlerAngles)
